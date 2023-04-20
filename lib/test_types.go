6ec40e46e2e059bfbb1c101f408530f6df63d143
Phani Raj <phani@planetscale.com>
2023-04-19 21:50:57 -0500

    add more test stubs

--- lib/test_types.go
+++ lib/test_types.go
@@ -50,20 +50,49 @@ func (c *clientConnectionMock) Sync(ctx context.Context, in *psdbconnect.SyncReq
 }
 
 type (
-	ReadFunc             func(ctx context.Context, logger DatabaseLogger, ps PlanetScaleSource, tableName string, tc *psdbconnect.TableCursor, onResult OnResult, onCursor OnCursor) (*SerializedCursor, error)
-	CanConnectFunc       func(ctx context.Context, ps PlanetScaleSource) error
-	ListVitessShardsFunc func(ctx context.Context, ps PlanetScaleSource) ([]string, error)
+	BuildSchemaFunc     func(ctx context.Context, psc PlanetScaleSource, schemaBuilder SchemaBuilder) error
+	PingContextFunc     func(context.Context, PlanetScaleSource) error
+	GetVitessShardsFunc func(ctx context.Context, psc PlanetScaleSource) ([]string, error)
+	CloseFunc           func() error
+	TestMysqlClient     struct {
+		BuildSchemaFn     BuildSchemaFunc
+		PingContextFn     PingContextFunc
+		GetVitessShardsFn GetVitessShardsFunc
+		CloseFn           CloseFunc
+	}
+)
+
+func (t TestMysqlClient) BuildSchema(ctx context.Context, psc PlanetScaleSource, schemaBuilder SchemaBuilder) error {
+	return t.BuildSchemaFn(ctx, psc, schemaBuilder)
+}
+
+func (t TestMysqlClient) PingContext(ctx context.Context, source PlanetScaleSource) error {
+	return t.PingContextFn(ctx, source)
+}
+
+func (t TestMysqlClient) GetVitessShards(ctx context.Context, psc PlanetScaleSource) ([]string, error) {
+	return t.GetVitessShardsFn(ctx, psc)
+}
+
+func (t TestMysqlClient) Close() error {
+	return t.CloseFn()
+}
+
+type (
+	ReadFunc       func(ctx context.Context, logger DatabaseLogger, ps PlanetScaleSource, tableName string, tc *psdbconnect.TableCursor, onResult OnResult, onCursor OnCursor) (*SerializedCursor, error)
+	CanConnectFunc func(ctx context.Context, ps PlanetScaleSource) error
+	ListShardsFunc func(ctx context.Context, ps PlanetScaleSource) ([]string, error)
 
 	TestConnectClient struct {
-		ReadFn             ReadFunc
-		CanConnectFn       CanConnectFunc
-		ListVitessShardsFn ListVitessShardsFunc
+		ReadFn       ReadFunc
+		CanConnectFn CanConnectFunc
+		ListShardsFn ListShardsFunc
 	}
 )
 
 func (tcc *TestConnectClient) ListShards(ctx context.Context, ps PlanetScaleSource) ([]string, error) {
-	if tcc.ListVitessShardsFn != nil {
-		return tcc.ListVitessShardsFn(ctx, ps)
+	if tcc.ListShardsFn != nil {
+		return tcc.ListShardsFn(ctx, ps)
 	}
 
 	panic("implement me")
@@ -73,7 +102,7 @@ func (tcc *TestConnectClient) CanConnect(ctx context.Context, ps PlanetScaleSour
 	if tcc.CanConnectFn != nil {
 		return tcc.CanConnectFn(ctx, ps)
 	}
-	return errors.New("Unimplemented")
+	return errors.New("CanConnect is Unimplemented")
 }
 
 func (tcc *TestConnectClient) Read(ctx context.Context, logger DatabaseLogger, ps PlanetScaleSource, tableName string, lastKnownPosition *psdbconnect.TableCursor, onResult OnResult, onCursor OnCursor) (*SerializedCursor, error) {
@@ -81,7 +110,7 @@ func (tcc *TestConnectClient) Read(ctx context.Context, logger DatabaseLogger, p
 		return tcc.ReadFn(ctx, logger, ps, tableName, lastKnownPosition, onResult, onCursor)
 	}
 
-	return nil, errors.New("Unimplemented")
+	return nil, errors.New("Read is Unimplemented")
 }
 
 func NewTestConnectClient(r ReadFunc) ConnectClient {
