c2c210e13a94439e955645f20fd139070c9f0874
Phani Raj <phani@planetscale.com>
2023-05-02 13:33:36 -0500

    Detect Decimal Scale & Precision for a myql Decimal column

--- cmd/internal/server/handlers/schema_builder.go
+++ cmd/internal/server/handlers/schema_builder.go
@@ -1,6 +1,10 @@
 package handlers
 
 import (
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
 	"strings"
 
 	"github.com/planetscale/fivetran-source/lib"
@@ -79,9 +83,11 @@ func (s *fivetranSchemaBuilder) OnColumns(keyspaceName, tableName string, column
 	}
 
 	for _, column := range columns {
+		dataType, decimalParams := getFivetranDataType(column.Type, s.treatTinyIntAsBoolean)
 		table.Columns = append(table.Columns, &fivetransdk.Column{
 			Name:       column.Name,
-			Type:       getFivetranDataType(column.Type, s.treatTinyIntAsBoolean),
+			Type:       dataType,
+			Decimal:    decimalParams,
 			PrimaryKey: column.IsPrimaryKey,
 		})
 	}
@@ -104,71 +110,71 @@ func (s *fivetranSchemaBuilder) BuildResponse() (*fivetransdk.SchemaResponse, er
 }
 
 // Convert columnType to fivetran type
-func getFivetranDataType(mType string, treatTinyIntAsBoolean bool) fivetransdk.DataType {
+func getFivetranDataType(mType string, treatTinyIntAsBoolean bool) (fivetransdk.DataType, *fivetransdk.DecimalParams) {
 	mysqlType := strings.ToLower(mType)
 	if strings.HasPrefix(mysqlType, "tinyint") {
 		if treatTinyIntAsBoolean {
-			return fivetransdk.DataType_BOOLEAN
+			return fivetransdk.DataType_BOOLEAN, nil
 		}
 
-		return fivetransdk.DataType_INT
+		return fivetransdk.DataType_INT, nil
 	}
 
 	// A BIT(n) type can have a length of 1 to 64
 	// we serialize these as a LONG : int64 value when serializing rows.
 	if strings.HasPrefix(mysqlType, "bit") {
-		return fivetransdk.DataType_LONG
+		return fivetransdk.DataType_LONG, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "varbinary") {
-		return fivetransdk.DataType_BINARY
+		return fivetransdk.DataType_BINARY, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "int") {
 		if strings.Contains(mysqlType, "unsigned") {
-			return fivetransdk.DataType_LONG
+			return fivetransdk.DataType_LONG, nil
 		}
-		return fivetransdk.DataType_INT
+		return fivetransdk.DataType_INT, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "smallint") {
-		return fivetransdk.DataType_INT
+		return fivetransdk.DataType_INT, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "bigint") {
-		return fivetransdk.DataType_LONG
+		return fivetransdk.DataType_LONG, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "decimal") {
-		return fivetransdk.DataType_DECIMAL
+		return fivetransdk.DataType_DECIMAL, getDecimalParams(mysqlType)
 	}
 
 	if strings.HasPrefix(mysqlType, "double") ||
 		strings.HasPrefix(mysqlType, "float") {
-		return fivetransdk.DataType_DOUBLE
+		return fivetransdk.DataType_DOUBLE, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "timestamp") {
-		return fivetransdk.DataType_UTC_DATETIME
+		return fivetransdk.DataType_UTC_DATETIME, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "time") {
-		return fivetransdk.DataType_STRING
+		return fivetransdk.DataType_STRING, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "datetime") {
-		return fivetransdk.DataType_NAIVE_DATETIME
+		return fivetransdk.DataType_NAIVE_DATETIME, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "year") {
-		return fivetransdk.DataType_INT
+		return fivetransdk.DataType_INT, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "varchar") ||
 		strings.HasPrefix(mysqlType, "text") ||
 		strings.HasPrefix(mysqlType, "enum") ||
 		strings.HasPrefix(mysqlType, "char") {
-		return fivetransdk.DataType_STRING
+		return fivetransdk.DataType_STRING, nil
 	}
 
 	if strings.HasPrefix(mysqlType, "set") ||
@@ -180,35 +186,78 @@ func getFivetranDataType(mType string, treatTinyIntAsBoolean bool) fivetransdk.D
 		strings.HasPrefix(mysqlType, "point") ||
 		strings.HasPrefix(mysqlType, "linestring") ||
 		strings.HasPrefix(mysqlType, "multilinestring") {
-		return fivetransdk.DataType_JSON
+		return fivetransdk.DataType_JSON, nil
 	}
 
 	switch mysqlType {
 	case "date":
-		return fivetransdk.DataType_NAIVE_DATE
+		return fivetransdk.DataType_NAIVE_DATE, nil
 	case "json":
-		return fivetransdk.DataType_JSON
+		return fivetransdk.DataType_JSON, nil
 	case "tinytext":
-		return fivetransdk.DataType_STRING
+		return fivetransdk.DataType_STRING, nil
 	case "mediumtext":
-		return fivetransdk.DataType_STRING
+		return fivetransdk.DataType_STRING, nil
 	case "longtext":
-		return fivetransdk.DataType_STRING
+		return fivetransdk.DataType_STRING, nil
 	case "binary":
-		return fivetransdk.DataType_BINARY
+		return fivetransdk.DataType_BINARY, nil
 	case "blob":
-		return fivetransdk.DataType_BINARY
+		return fivetransdk.DataType_BINARY, nil
 	case "longblob":
-		return fivetransdk.DataType_BINARY
+		return fivetransdk.DataType_BINARY, nil
 	case "mediumblob":
-		return fivetransdk.DataType_BINARY
+		return fivetransdk.DataType_BINARY, nil
 	case "tinyblob":
-		return fivetransdk.DataType_BINARY
+		return fivetransdk.DataType_BINARY, nil
 	case "bit":
-		return fivetransdk.DataType_BOOLEAN
+		return fivetransdk.DataType_BOOLEAN, nil
 	case "time":
-		return fivetransdk.DataType_STRING
+		return fivetransdk.DataType_STRING, nil
 	default:
-		return fivetransdk.DataType_STRING
+		return fivetransdk.DataType_STRING, nil
+	}
+}
+
+// getDecimalParams parses the mysql type declaration for a column
+// and returns a type compatible with fivetran's SDK
+func getDecimalParams(mysqlType string) *fivetransdk.DecimalParams {
+
+	// From : https://dev.mysql.com/doc/refman/5.7/en/precision-math-decimal-characteristics.html
+	// The declaration syntax for a DECIMAL column is DECIMAL(M,D). The ranges of values for the arguments are as follows:
+	// M is the maximum number of digits (the precision). It has a range of 1 to 65.
+	var m uint32 = 10
+	// D is the number of digits to the right of the decimal point (the scale). It has a range of 0 to 30 and must be no larger than M.
+	// If D is omitted, the default is 0. If M is omitted, the default is 10.
+	var d uint32 = 0
+
+	precision := strings.Replace(strings.ToUpper(mysqlType), "DECIMAL", "", 1)
+	r := regexp.MustCompile(`[-]?\d[\d,]*[\d{2}]*`)
+	matches := r.FindAllString(precision, -1)
+	fmt.Printf("matches is %s\n", matches)
+	if len(matches) == 0 {
+		fmt.Println("returning defaults")
+		// return defaults
+		return &fivetransdk.DecimalParams{
+			Precision: m,
+			Scale:     d,
+		}
+	}
+
+	parts := strings.Split(matches[0], ",")
+	if len(parts) > 1 {
+		di, err := strconv.ParseUint(parts[1], 10, 32)
+		if err == nil && di <= math.MaxUint32 {
+			d = uint32(di)
+		}
+	}
+
+	mi, err := strconv.ParseUint(parts[0], 10, 32)
+	if err == nil && mi <= math.MaxUint32 {
+		m = uint32(mi)
+	}
+	return &fivetransdk.DecimalParams{
+		Precision: m,
+		Scale:     d,
 	}
 }
