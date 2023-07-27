af37b5483c69e5f9a5592e0756575c6af211cb8d
notfelineit <notfelineit@gmail.com>
2023-07-27 10:56:11 -0700

    Use new sdk

--- cmd/internal/server/handlers/schema.go
+++ cmd/internal/server/handlers/schema.go
@@ -3,14 +3,13 @@ package handlers
 import (
 	"context"
 
+	fivetransdk_v2 "github.com/planetscale/fivetran-sdk-grpc/go"
 	"github.com/planetscale/fivetran-source/lib"
-
-	fivetransdk "github.com/planetscale/fivetran-proto/go"
 )
 
 type Schema struct{}
 
-func (Schema) Handle(ctx context.Context, psc *lib.PlanetScaleSource, db *lib.MysqlClient) (*fivetransdk.SchemaResponse, error) {
+func (Schema) Handle(ctx context.Context, psc *lib.PlanetScaleSource, db *lib.MysqlClient) (*fivetransdk_v2.SchemaResponse, error) {
 	schemaBuilder := NewSchemaBuilder(psc.TreatTinyIntAsBoolean)
 	if err := (*db).BuildSchema(ctx, *psc, schemaBuilder); err != nil {
 		return nil, err
