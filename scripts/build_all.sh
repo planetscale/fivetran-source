bca29ef7e4dbb6c17174b97395e92d193b11a267
Phani Raj <phani@planetscale.com>
2023-12-14 05:30:44 -0600

    fix CI to clone proto and generate types

--- scripts/build_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf proto
-git clone https://github.com/fivetran/fivetran_sdk proto/
-scripts/go.sh
--- scripts/go.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p fivetran_sdk
-protoc \
-    --go_out=fivetran_sdk \
-    --go_opt=paths=source_relative \
-    --go-grpc_out=fivetran_sdk \
-    --proto_path=proto \
-    --go-grpc_opt=paths=source_relative \
-    common.proto \
-    connector_sdk.proto
